58a68cdc60dac5aac2d1b0bd80aa184151bad17e
Freddie <whatis@tsdo.in>
2023-04-30 00:45:55 +0800

    interpolation

--- src/panorama/manifest.ts
+++ src/panorama/manifest.ts
@@ -1,4 +1,203 @@
-$.Msg("ui manifest loaded");
+type Time = number;
+
+const lerp = (start: number, end: number, t: number) => {
+    return start + (end - start) * t;
+};
+
+interface Snapshot {
+    time: Time;
+    state: CursorPos;
+}
+
+interface InterpolatedSnapshot {
+    state: CursorPos;
+    percentage: number;
+}
+
+class Vault {
+    private _vault: Snapshot[] = [];
+    private _vaultSize: number = 120;
+
+    /** Clear this Vault */
+    clear(): void {
+        this._vault = [];
+    }
+
+    /** Get the latest snapshot */
+    get(): Snapshot | undefined;
+    /** Get the two snapshots around a specific time */
+    get(time: number): { older: Snapshot; newer: Snapshot } | undefined;
+    /** Get the closest snapshot to e specific time */
+    get(time: number, closest: boolean): Snapshot | undefined;
+
+    get(time?: number, closest?: boolean) {
+        // zero index is the newest snapshot
+        const sorted = this._vault.sort((a, b) => b.time - a.time);
+        if (typeof time === "undefined") return sorted[0];
+
+        for (let i = 0; i < sorted.length; i++) {
+            const snap = sorted[i];
+            if (snap.time <= time) {
+                const snaps = { older: sorted[i], newer: sorted[i - 1] };
+                if (closest) {
+                    const older = Math.abs(time - snaps.older.time);
+                    const newer = Math.abs(time - snaps.newer?.time);
+                    if (isNaN(newer)) return snaps.older;
+                    else if (newer <= older) return snaps.older;
+                    else return snaps.newer;
+                }
+                return snaps;
+            }
+        }
+        return;
+    }
+
+    /** Add a snapshot to the vault. */
+    add(snapshot: Snapshot) {
+        if (this._vault.length > this._vaultSize - 1) {
+            // remove the oldest snapshot
+            this._vault.sort((a, b) => a.time - b.time).shift();
+        }
+        this._vault.push(snapshot);
+    }
+
+    /** Get the current capacity (size) of the vault. */
+    public get size() {
+        return this._vault.length;
+    }
+
+    /** Set the max capacity (size) of the vault. */
+    setMaxSize(size: number) {
+        this._vaultSize = size;
+    }
+
+    /** Get the max capacity (size) of the vault. */
+    getMaxSize() {
+        return this._vaultSize;
+    }
+}
+
+class SnapshotInterpolation {
+    /** Access the vault. */
+    public vault = new Vault();
+    private _interpolationBuffer = (1000 / 30) * 2;
+    private _timeOffset = -1;
+    /** The current server time based on the current snapshot interpolation. */
+    public serverTime = 0;
+
+    public get interpolationBuffer() {
+        return {
+            /** Get the Interpolation Buffer time in milliseconds. */
+            get: () => this._interpolationBuffer,
+            /** Set the Interpolation Buffer time in milliseconds. */
+            set: (milliseconds: number) => {
+                this._interpolationBuffer = milliseconds;
+            },
+        };
+    }
+
+    public static Now() {
+        return Game.GetGameTime() * 1000;
+    }
+
+    public get timeOffset() {
+        return this._timeOffset;
+    }
+
+    public get snapshot() {
+        return {
+            create: (state: CursorPos): Snapshot =>
+                SnapshotInterpolation.CreateSnapshot(state),
+            add: (snapshot: Snapshot): void => this.addSnapshot(snapshot),
+        };
+    }
+
+    public static CreateSnapshot(state: CursorPos): Snapshot {
+        return {
+            time: SnapshotInterpolation.Now(),
+            state: state,
+        };
+    }
+
+    private addSnapshot(snapshot: Snapshot): void {
+        const timeNow = SnapshotInterpolation.Now();
+        const timeSnapshot = snapshot.time;
+
+        if (this._timeOffset === -1) {
+            this._timeOffset = timeNow - timeSnapshot;
+        }
+
+        // correct time offset
+        const timeOffset = timeNow - timeSnapshot;
+        const timeDifference = Math.abs(this._timeOffset - timeOffset);
+        if (timeDifference > 50) this._timeOffset = timeOffset;
+
+        this.vault.add(snapshot);
+    }
+
+    /** Interpolate between two snapshots give the percentage or time. */
+    public interpolate(
+        snapshotA: Snapshot,
+        snapshotB: Snapshot,
+        timeOrPercentage: number
+    ): InterpolatedSnapshot {
+        return this._interpolate(snapshotA, snapshotB, timeOrPercentage);
+    }
+
+    private _interpolate(
+        snapshotA: Snapshot,
+        snapshotB: Snapshot,
+        timeOrPercentage: number
+    ): InterpolatedSnapshot {
+        const sorted = [snapshotA, snapshotB].sort((a, b) => b.time - a.time);
+
+        const newer: Snapshot = sorted[0];
+        const older: Snapshot = sorted[1];
+
+        const t0: Time = newer.time;
+        const t1: Time = older.time;
+        const tn: number = timeOrPercentage;
+
+        const zeroPercent = tn - t1;
+        const hundredPercent = t0 - t1;
+        const pPercent =
+            timeOrPercentage <= 1
+                ? timeOrPercentage
+                : zeroPercent / hundredPercent;
+
+        this.serverTime = lerp(t1, t0, pPercent);
+
+        const lerpFnc = (start: CursorPos, end: CursorPos, t: number) => {
+            return vadd3(start, vmul3(vsub3(end, start), t));
+        };
+
+        const newerState: CursorPos = newer.state;
+        const olderState: CursorPos = older.state;
+
+        const interpolatedSnapshot: InterpolatedSnapshot = {
+            state: lerpFnc(olderState, newerState, pPercent),
+            percentage: pPercent,
+        };
+
+        return interpolatedSnapshot;
+    }
+
+    /** Get the calculated interpolation on the client. */
+    public calcInterpolation(): InterpolatedSnapshot | undefined {
+        const serverTime =
+            SnapshotInterpolation.Now() -
+            this._timeOffset -
+            this._interpolationBuffer;
+
+        const shots = this.vault.get(serverTime);
+        if (!shots) return;
+
+        const { older, newer } = shots;
+        if (!older || !newer) return;
+
+        return this._interpolate(newer, older, serverTime);
+    }
+}
 
 type UpdateCallback = (time: number, deltaTime: number) => void;
 
@@ -15,9 +214,6 @@ const runFrames = () => {
 
 $.Schedule(0, runFrames);
 
-const SPEED = 20;
-const STABILIZATION = 0.75;
-
 const vsub = (a: [number, number], b: [number, number]): [number, number] => [
     a[0] - b[0],
     a[1] - b[1],
@@ -26,22 +222,44 @@ const vadd = (a: [number, number], b: [number, number]): [number, number] => [
     a[0] + b[0],
     a[1] + b[1],
 ];
-const vmul = (a: [number, number], b: number): [number, number] => [a[0] * b, a[1] * b];
-const vdiv = (a: [number, number], b: number): [number, number] => [a[0] / b, a[1] / b];
-const vlen = (a: [number, number]): number => Math.sqrt(a[0] * a[0] + a[1] * a[1]);
+const vmul = (a: [number, number], b: number): [number, number] => [
+    a[0] * b,
+    a[1] * b,
+];
+const vdiv = (a: [number, number], b: number): [number, number] => [
+    a[0] / b,
+    a[1] / b,
+];
+const vlen = (a: [number, number]): number =>
+    Math.sqrt(a[0] * a[0] + a[1] * a[1]);
+
+const vsub3 = (a: CursorPos, b: CursorPos): CursorPos => [
+    a[0] - b[0],
+    a[1] - b[1],
+    a[2] - b[2],
+];
+const vadd3 = (a: CursorPos, b: CursorPos): CursorPos => [
+    a[0] + b[0],
+    a[1] + b[1],
+    a[2] + b[2],
+];
+const vmul3 = (a: CursorPos, b: number): CursorPos => [
+    a[0] * b,
+    a[1] * b,
+    a[2] * b,
+];
+const vdiv3 = (a: CursorPos, b: number): CursorPos => [
+    a[0] / b,
+    a[1] / b,
+    a[2] / b,
+];
+const vlen3 = (a: CursorPos): number =>
+    Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
 
 class CursorInstance {
-    private cursor: Panel;
-    private lastPos: [number, number] = [0, 0];
-    private lastTime: number = 0;
-    private currentPos: [number, number] = [0, 0];
-    private currentTime: number = 0;
     private id: PlayerID;
-
-    // custom smoothing
-    private lastDrawPos: [number, number] = [0, 0];
-    private drawPos: [number, number] = [0, 0];
-    private entropy = 0.5;
+    private cursor: Panel;
+    private snapshotInterpolation: SnapshotInterpolation;
 
     constructor(id: PlayerID) {
         this.id = id;
@@ -53,6 +271,8 @@ class CursorInstance {
         this.cursor.AddClass("SharedCursor");
 
         const color = Players.GetPlayerColor(id);
+        const enemy =
+            Players.GetTeam(id) !== Players.GetTeam(Players.GetLocalPlayer());
         const ABGR = color.toString(16);
         const RGBA =
             ABGR[6] +
@@ -64,6 +284,10 @@ class CursorInstance {
             ABGR[0] +
             ABGR[1];
         this.cursor.style.washColor = `#${RGBA}`;
+        this.snapshotInterpolation = new SnapshotInterpolation();
+        if (enemy) {
+            this.cursor.AddClass("Enemy");
+        }
 
         frameEvents[id] = (time, deltaTime) => {
             this.draw(time, deltaTime);
@@ -71,51 +295,31 @@ class CursorInstance {
     }
 
     public update(x: number, y: number, z: number, t: number) {
-        const sx = Game.WorldToScreenX(x, y, z);
-        const sy = Game.WorldToScreenY(x, y, z);
-
-        const scaleFactor = 1080 / Game.GetScreenHeight();
-        const scaleX = sx * scaleFactor;
-        const scaleY = sy * scaleFactor;
-
-        this.lastPos = this.currentPos;
-        this.lastTime = this.currentTime;
-        this.currentPos = [scaleX, scaleY];
-        this.currentTime = t;
+        const pos: CursorPos = [x, y, z];
+        this.snapshotInterpolation.snapshot.add({
+            time: t * 1000,
+            state: pos,
+        });
     }
 
     public draw(time: number, deltaTime: number) {
-        const lastX = this.lastPos[0];
-        const lastY = this.lastPos[1];
+        const result = this.snapshotInterpolation.calcInterpolation();
 
-        const x = this.currentPos[0];
-        const y = this.currentPos[1];
-
-        const elapsed = this.currentTime - this.lastTime;
-
-        if (elapsed > 0.0001) {
-            const extrapolatedX =
-                x + ((x - lastX) * (time - this.currentTime)) / elapsed;
-            const extrapolatedY =
-                y + ((y - lastY) * (time - this.currentTime)) / elapsed;
-
-            const targetValue: [number, number] = [extrapolatedX, extrapolatedY];
-
-            this.entropy += SPEED * deltaTime;
-            this.entropy -=
-                vlen(vsub(targetValue, this.lastDrawPos)) *
-                Math.log10(STABILIZATION + 1.0) *
-                0.02;
-            this.entropy = Math.min(Math.max(this.entropy, 0.0), SPEED);
-
-            this.drawPos = vadd(this.drawPos, 
-                vmul(vsub(targetValue, this.drawPos),
-                Math.min(Math.max(deltaTime * this.entropy, 0.0), 1.0));
-            this.lastDrawPos = targetValue;
-
-            this.cursor.style.position = `${this.drawPos[0] + 50}px ${
-                this.drawPos[1] + 50
-            }px 0px`;
+        if (result) {
+            const sx = Game.WorldToScreenX(
+                result.state[0],
+                result.state[1],
+                result.state[2]
+            );
+            const sy = Game.WorldToScreenY(
+                result.state[0],
+                result.state[1],
+                result.state[2]
+            );
+            const scaleFactor = 1080 / Game.GetScreenHeight();
+            const x = sx * scaleFactor;
+            const y = sy * scaleFactor;
+            this.cursor.style.position = `${x}px ${y}px 0px`;
         }
     }
 
--- src/vscripts/GameMode.ts
+++ src/vscripts/GameMode.ts
@@ -9,18 +9,5 @@ declare global {
 export class GameMode {
     public static Precache(this: void, context: CScriptPrecacheContext) {}
 
-    public static Activate(this: void) {
-        // When the addon activates, create a new instance of this GameMode class.
-        GameRules.Addon = new GameMode();
-
-        if (IsServer()) {
-            
-        }
-
-        if (IsClient()) {
-           
-        }
-    }
-
-    constructor() {}
+    public static Activate(this: void) {}
 }
